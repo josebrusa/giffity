30067ac3b5534f5341921419d44a39f9297f85cf
jose Brusa <josebrusa@outlook.com>
2021-04-29 01:05:11 -0300

    midu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ListOfGifs from './components/ListOfGifs'
+import { Link, Route } from 'wouter'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>App</h1>
+
+          <Link to='/gif/dragonball'>Dragon Ball</Link>
+          <Link to='/gif/lossimpsons'>Los Simpsons</Link>
+          <Link to='/gif/powerrangers'>Power Rangers</Link>
+
+        <Route component={ListOfGifs} path='/gif/:keyword' />
+        {/* <ListOfGifs keyword='panda' /> */}
+        {/* <button onClick={() => setGifs(DIFERENT_GIFS)}>Cambiar gif</button> */}
+      </section>
     </div>
   );
 }
