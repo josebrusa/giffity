88c4380e015e8d720edcc70b038f1712046985ab
jose Brusa <josebrusa@outlook.com>
2021-05-17 10:06:44 -0300

    agregamos scroll infinito y styles

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,17 +1,20 @@
 import { useEffect, useState, useRef } from 'react'
 
-export default function useNearScreen ({ distance= '100px' }) {
+export default function useNearScreen ({ distance= '100px', externalRef, once = true } = {} ) {
     const [isNearScreen, setShow] = useState(false)
     const fromRef = useRef()
 
 
     useEffect(function(){
         let observer
+        const element = externalRef ? externalRef.current : fromRef.current
         const onChange = (entries, observer) => {
             const el = entries[0]
             if (el.isIntersecting) {
                 setShow(true)
-                observer.disconnect()
+                once && observer.disconnect()
+            } else {
+                !once && setShow(false)
             }
         }
         Promise.resolve(
@@ -22,10 +25,10 @@ export default function useNearScreen ({ distance= '100px' }) {
             observer = new IntersectionObserver(onChange, {
                 rootMargin: distance
             })
-            observer.observe(fromRef.current)
+            if (element) observer.observe( element )
         })
 
-        return () => observer.disconnect()
+        return () => observer && observer.disconnect()
     })
     return {isNearScreen, fromRef}
 }
