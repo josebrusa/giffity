930f6ced81f58b204d03c58983c869a3f9dfb774
jose Brusa <josebrusa@outlook.com>
2021-05-25 22:43:23 -0300

    add error page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React, { Suspense } from 'react';
-import './App.css'
-import SearchResults from './pages/SearchResults'
-import Detail from './pages/Detail'
-import Pepito from './context/StaticContext'
-import {GifsContextProvider} from './context/GifsContext'
-import { Link, Route } from "wouter"
+import React, { Suspense } from "react";
+import { Link, Route, Switch } from "wouter";
 import logo from './isologo.svg'
+import Header from "components/Header";
 
-const HomePage = React.lazy(() => import('./pages/Home'))
+
+import SearchResults from "pages/SearchResults";
+import Detail from "pages/Detail";
+import ErrorPage from "pages/ErrorPage";
+
+import { GifsContextProvider } from "context/GifsContext";
+
+import "./App.css";
+
+const HomePage = React.lazy(() => import("./pages/Home"));
 
 export default function App() {
   return (
-  <Pepito.Provider value={{name: 'cocho',
-      suscribeteAlCanal: true}}>
-          <div className="App">
-          <Suspense fallback={null}>
-            <section className="App-content">
-              <Link to="/">
-                <figure className="App-logo">
-                  <img alt='giffity isologo' src={logo} />
-                </figure>
-              </Link>
-              <GifsContextProvider>
+      <div className="App">
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Header />
+            <Link to="/">
+              <figure className="App-logo">
+              <img alt='giffity isologo' src={logo} />
+              </figure>
+            </Link>
+            <GifsContextProvider>
+              <Switch>
                 <Route component={HomePage} path="/" />
-                <Route component={SearchResults} path="/search/:keyword/:rating?" />
+                <Route
+                  component={SearchResults}
+                  path="/search/:keyword/:rating?"
+                />
                 <Route component={Detail} path="/gif/:id" />
-                <Route component={() => <h1>Ups! Error 404 / Preguntale a Cocho que paso 😁</h1>} path='/404' />
-              </GifsContextProvider>
-            </section>
-            </Suspense>
-          </div>
-    </Pepito.Provider>
-  )
+                <Route component={ErrorPage} path="/:rest*" />
+              </Switch>
+            </GifsContextProvider>
+          </section>
+        </Suspense>
+      </div>
+  );
 }
-
